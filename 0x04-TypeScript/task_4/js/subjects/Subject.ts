eed21ba1ee7f797b8883877ba7999abc8840f61c
Emmanuel <blaemedia26@gmail.com>
2025-11-06 04:22:48 +0100

    Update Subject.ts implementation

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,23 +1,14 @@
-// Subject.ts
 namespace Subjects {
-  // Teacher interface (should already exist in Teacher.ts)
-  export interface Teacher {
-    firstName: string;
-    lastName: string;
-  }
-
-  // Subject class
-  export class Subject {
-    private _teacher!: Teacher; // instance attribute teacher
-
-    // Setter method to set the teacher
-    setTeacher(teacher: Teacher): void {
-      this._teacher = teacher;
+    export interface Teacher {
+        firstName: string;
+        lastName: string;
     }
 
-    // Optional getter if you want to access the teacher
-    get teacher(): Teacher {
-      return this._teacher;
+    export class Subject {
+        teacher: Teacher;
+
+        setTeacher(teacher: Teacher): void {
+            this.teacher = teacher;
+        }
     }
-  }
 }
